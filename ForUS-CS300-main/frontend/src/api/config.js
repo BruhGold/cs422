88d55f1b4807690209981651269358a5f5c89315
Ngô Hà Định <aawsnd@gmail.com>
2024-05-21 09:41:51 +0700

    deployeyryeryaerea

--- ForUS-CS300-main/frontend/src/api/config.js
+++ ForUS-CS300-main/frontend/src/api/config.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Set config defaults when creating the instance
 const instance = axios.create({
   withCredentials: true,
-  baseURL: "https://cs422.onrender.com",
+  baseURL: "https://cs422.onrender.com/18.142.128.26",
   headers: { "Content-Type": "application/json", Accept: "application/json" },
 });
 
